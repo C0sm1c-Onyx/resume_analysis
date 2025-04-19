e9d8cb6db093fb9b63252aa673a7a7c3fc8d24a0
Cosmic Onyx <c0sm1c0nyx.225@gmail.com>
2025-04-19 19:29:20 +0300

    init

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
 from seatable_api import context
 
 
-openai_key="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"
+openai_key=""
 
-proxyurl = "socks5://XkugDq:BcYCWw@95.164.202.188:9775"
-seatable_api_prompts = context.api_token or '610459a58d6702513c991dcb2692edbe252c0b12'
+proxyurl = ""
+seatable_api_prompts = context.api_token or ''
